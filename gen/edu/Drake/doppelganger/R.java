7c14e1d71f5efa4b4dfd9ea62f81cad0960defd6
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2013-04-01 16:55:11 -0500

    this had better work

--- gen/edu/Drake/doppelganger/R.java
+++ gen/edu/Drake/doppelganger/R.java
@@ -15,18 +15,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-<<<<<<< HEAD
+        public static final int button_capture=0x7f080018;
+        public static final int button_retake=0x7f080017;
+        public static final int button_use=0x7f080019;
+        public static final int camera_preview=0x7f080016;
         public static final int editText1=0x7f080011;
         public static final int editText2=0x7f080013;
         public static final int editText3=0x7f080012;
         public static final int editText4=0x7f080014;
         public static final int editText5=0x7f080015;
-=======
-        public static final int button_capture=0x7f08000f;
-        public static final int button_retake=0x7f08000e;
-        public static final int button_use=0x7f080010;
-        public static final int camera_preview=0x7f08000d;
->>>>>>> myContribution
         public static final int email=0x7f080006;
         public static final int fragment_container=0x7f08000a;
         public static final int imageView1=0x7f080000;
@@ -35,17 +32,10 @@ public final class R {
         public static final int login_form=0x7f080005;
         public static final int login_status=0x7f080003;
         public static final int login_status_message=0x7f080004;
-<<<<<<< HEAD
-        public static final int menu_cancel=0x7f080017;
-        public static final int menu_compose=0x7f080019;
-        public static final int menu_forgot_password=0x7f080018;
-        public static final int menu_settings=0x7f080016;
-=======
-        public static final int menu_cancel=0x7f080012;
-        public static final int menu_compose=0x7f080014;
-        public static final int menu_forgot_password=0x7f080013;
-        public static final int menu_settings=0x7f080011;
->>>>>>> myContribution
+        public static final int menu_cancel=0x7f08001b;
+        public static final int menu_compose=0x7f08001d;
+        public static final int menu_forgot_password=0x7f08001c;
+        public static final int menu_settings=0x7f08001a;
         public static final int password=0x7f080007;
         public static final int select_celeb=0x7f08000c;
         public static final int select_pic=0x7f08000b;
@@ -64,13 +54,9 @@ public final class R {
         public static final int activity_more_info=0x7f030004;
         public static final int activity_notifications=0x7f030005;
         public static final int activity_post=0x7f030006;
-<<<<<<< HEAD
         public static final int activity_register=0x7f030007;
         public static final int activity_take_picture=0x7f030008;
-=======
-        public static final int activity_take_picture=0x7f030007;
-        public static final int main=0x7f030008;
->>>>>>> myContribution
+        public static final int main=0x7f030009;
     }
     public static final class menu {
         public static final int activity_celebrities=0x7f070000;
@@ -79,13 +65,9 @@ public final class R {
         public static final int activity_more_info=0x7f070003;
         public static final int activity_notifications=0x7f070004;
         public static final int activity_post=0x7f070005;
-<<<<<<< HEAD
         public static final int activity_register=0x7f070006;
         public static final int activity_take_picture=0x7f070007;
-=======
-        public static final int activity_take_picture=0x7f070006;
-        public static final int main=0x7f070007;
->>>>>>> myContribution
+        public static final int main=0x7f070008;
     }
     public static final class raw {
         public static final int celebrities=0x7f040000;
@@ -100,31 +82,30 @@ public final class R {
         public static final int zooey=0x7f040009;
     }
     public static final class string {
-        public static final int action_sign_in_register=0x7f05000d;
-        public static final int action_sign_in_short=0x7f05000e;
+        public static final int action_sign_in_register=0x7f05000e;
+        public static final int action_sign_in_short=0x7f05000f;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050014;
-        public static final int error_incorrect_password=0x7f050013;
-        public static final int error_invalid_email=0x7f050011;
-        public static final int error_invalid_password=0x7f050012;
+        public static final int error_field_required=0x7f050015;
+        public static final int error_incorrect_password=0x7f050014;
+        public static final int error_invalid_email=0x7f050012;
+        public static final int error_invalid_password=0x7f050013;
         public static final int hello_world=0x7f050001;
-        public static final int login_progress_signing_in=0x7f050010;
-        public static final int menu_forgot_password=0x7f05000f;
+        public static final int login_progress_signing_in=0x7f050011;
+        public static final int menu_forgot_password=0x7f050010;
         public static final int menu_settings=0x7f050002;
         public static final int name_string=0x7f050003;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000b;
-        public static final int prompt_password=0x7f05000c;
-<<<<<<< HEAD
-=======
-        public static final int title_activity_camerademo=0x7f050009;
->>>>>>> myContribution
+        public static final int prompt_email=0x7f05000c;
+        public static final int prompt_password=0x7f05000d;
+        public static final int title_activity_camerademo=0x7f05000a;
         public static final int title_activity_celebrities=0x7f050007;
-        public static final int title_activity_login=0x7f05000a;
+        public static final int title_activity_login=0x7f05000b;
         public static final int title_activity_more_info=0x7f050004;
         public static final int title_activity_notifications=0x7f050008;
         public static final int title_activity_post=0x7f050005;
+        /** <<<<<<< HEAD 
+         */
         public static final int title_activity_register=0x7f050009;
         public static final int title_activity_take_picture=0x7f050006;
     }
