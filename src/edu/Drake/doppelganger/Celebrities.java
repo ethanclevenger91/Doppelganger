ac81911e5c5a32813361f5b1b592e7d00c872db8
Clayton <cjbrady09@gmail.com>
2013-04-02 09:52:52 -0500

    added stuff in notifications

--- src/edu/Drake/doppelganger/Celebrities.java
+++ src/edu/Drake/doppelganger/Celebrities.java
@@ -1,9 +1,10 @@
 package edu.Drake.doppelganger;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Celebrities extends Activity {
 
@@ -21,6 +22,10 @@ public class Celebrities extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_celebrities, menu);
 		return true;
+	}
+	
+	public void useAsDoppelganger(View v) {
+		onBackPressed();
 	}
 
 	@Override
