f27cd533e1ae59c7fe7d3265421e22297799a345
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2013-04-02 09:51:04 -0500

    this might fuck things up

--- src/edu/Drake/doppelganger/RegisterActivity.java
+++ src/edu/Drake/doppelganger/RegisterActivity.java
@@ -2,24 +2,29 @@ package edu.Drake.doppelganger;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends Activity {
 
+	ImageButton start;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register);
-		
-		//Pull email address from intent to auto-fill it if registering due to failed login
-		Bundle extras = getIntent().getExtras();
-		if (extras != null) {
-		    String email = extras.getString("email");
-		    EditText emailView = (EditText) findViewById(R.id.editText3);
-		    emailView.setText(email);
-		}
-		
+		start = (ImageButton) findViewById(R.id.start);
+		start.setOnClickListener(new OnClickListener() {
+			public void onClick(View v)
+			{
+				Intent start = new Intent(RegisterActivity.this, MainActivity.class);
+				startActivity(start);
+			}
+		});
 	}
 
 	@Override
