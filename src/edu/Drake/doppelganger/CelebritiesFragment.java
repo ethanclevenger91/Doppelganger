b14209c75b830192eebd29c0923e94ddc007bea8
Ethan Clevenger <ethan.c.clevenger@gmail.com>
2013-04-22 16:18:42 -0500

    tried to do something. didnt work out

--- src/edu/Drake/doppelganger/CelebritiesFragment.java
+++ src/edu/Drake/doppelganger/CelebritiesFragment.java
@@ -7,18 +7,20 @@ import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 public class CelebritiesFragment extends ListFragment {
 
-	//@Override
-    //public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	/*@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 	 // Inflate the layout for this fragment
-    //return inflater.inflate(R.layout.activity_celebrities, container, false);
-//s}
+    return inflater.inflate(R.layout.activity_celebrities, null);
+}*/
 	
 		
 	@Override
